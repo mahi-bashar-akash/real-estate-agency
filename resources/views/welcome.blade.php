c5900cf96a1e89182fca12a110bef9e8cc2d2632
mahi-bashar-akash <mahibashar2023@gmail.com>
2025-02-12 19:01:25 +0600

    build-and-deploy

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,12 @@
 <!doctype html>
 <html lang="en">
 <head>
-
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
     <link rel="shortcut icon" href="{{asset('/images/logo.svg')}}" type="image/x-icon">
     <link rel="apple-touch-icon" href="{{asset('/images/logo.svg')}}">
     <title> Real Estate Agency </title>
-
     <meta name="description" content="Find your dream home with Real Estate Agency. Browse listings, buy, sell, or rent properties with ease. Expert agents, best deals, and seamless transactions.">
     <meta name="keywords" content="real estate agency, buy house, sell property, rent apartment, real estate listings, dream home, investment properties, commercial real estate, property management, luxury homes">
     <meta name="subject" content="Your Trusted Real Estate Agency">
@@ -18,20 +15,17 @@
     <meta name="identifier-URL" content="{{url()->current()}}">
     <meta name="author" content="Real Estate Agency">
     <meta name="robots" content="index, follow">
-
     <meta property="og:site_name" content="Real Estate Agency">
     <meta property="og:title" content="Real Estate Agency - Find Your Dream Home">
     <meta property="og:type" content="website">
     <meta property="og:url" content="{{url()->current()}}">
     <meta property="og:description" content="Find your dream home with Real Estate Agency. Browse listings, buy, sell, or rent properties with ease.">
     <meta property="og:image" content="{{asset('/images/logo.svg')}}">
-
     <meta name="twitter:card" content="summary_large_image">
     <meta property="twitter:url" content="{{url()->current()}}">
     <meta property="twitter:title" content="Real Estate Agency - Find Your Dream Home">
     <meta property="twitter:description" content="Find your dream home with Real Estate Agency. Browse listings, buy, sell, or rent properties with ease.">
     <meta property="twitter:image" content="{{asset('/images/logo.svg')}}">
-
     <meta name="google-site-verification" content="YOUR_GOOGLE_VERIFICATION_CODE"/>
     <script type="application/ld+json">
         {
@@ -58,12 +52,6 @@
           ],
         }
     </script>
-
-
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
 </head>
