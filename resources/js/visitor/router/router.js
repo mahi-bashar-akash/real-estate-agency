1df36a11f158b21b78f8fb0c18292d70dcb9cac8
mahi-bashar-akash <mahibashar2023@gmail.com>
2025-03-03 10:56:34 +0600

    build-and-deploy

--- resources/js/visitor/router/router.js
+++ resources/js/visitor/router/router.js
@@ -1,23 +1,17 @@
 import {createRouter, createWebHistory} from "vue-router";
 
 import Layout from "../portal/layout/layout.vue";
-
     import Index from "../portal/pages/index.vue";
     import About from "../portal/pages/about.vue";
-
     import Teams from "../portal/pages/teams.vue";
     import TeamDetails from "../portal/pages/team-details.vue";
-
     import Blogs from "../portal/pages/blogs.vue";
     import BlogDetails from "../portal/pages/blog-details.vue";
-
     import Properties from "../portal/pages/properties.vue";
     import PropertyDetails from "../portal/pages/property-details.vue";
-
     import ServiceDetails from "../portal/pages/service-details.vue";
 
 import AuthLayout from "../auth/layout/layout.vue";
-
     import SignIn from "../auth/pages/sign-in.vue";
     import SignUp from "../auth/pages/sign-up.vue";
     import Forgot from "../auth/pages/forgot.vue";
@@ -25,9 +19,10 @@ import AuthLayout from "../auth/layout/layout.vue";
 
 import Chat from "../chat/chats.vue";
 
-    const company_name = "Realer Living Solution";
+const company_name = "Realer Living Solution";
 
 const routes = [
+    // visitor part
     {path: '/', name: 'Layout', component: Layout,
         children: [
             {path: '', name: 'Index', component: Index, meta: {title: company_name + ' Home'}},
@@ -41,6 +36,7 @@ const routes = [
             {path: 'service-details/:id', name: 'ServiceDetails', component: ServiceDetails, meta: {title: company_name + ' Service Details'}},
         ]
     },
+    // authentication part
     {path: '/auth/', name: 'AuthLayout', component: AuthLayout,
         children: [
             {path: 'sign-in', name: 'SignIn', component: SignIn, meta: {title: company_name + ' Sign In'}},
@@ -49,7 +45,9 @@ const routes = [
             {path: 'reset', name: 'Reset', component: Reset, meta: {title: company_name + ' Reset'}},
         ]
     },
+    // chat part
     {path: '/chat', name: 'Chat', component: Chat, meta: {title: company_name + ' Chat'}},
+
 ];
 
 const router = createRouter({
